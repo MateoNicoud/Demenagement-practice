c3e517acbcd0a8d2975069341ebf471eb72e310b
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-24 10:58:56 +0200

    added Demenagement

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,35 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int TotalEntreprise1 = 34;
+        int TotalEntreprise2 = 0;
+        int i=0;
+        float[] tab = new float[TotalEntreprise1];
+        int voyage=0;
+        int carton=0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        while (TotalEntreprise2<34) {
+            //le dernier voyage et déchargement
+            if (i>33) {
+                voyage++;
+                TotalEntreprise2+= carton;
+                carton=0;
+            }
+            //chargement du camion et estimation du poids
+            else if (carton<9) {
+                carton++;
+                i++;
+                TotalEntreprise1--;
+            } else {
+                voyage++;
+                TotalEntreprise2+= carton;
+                carton=0;
+            }
         }
+
+        System.out.println(voyage);
+        System.out.println(TotalEntreprise1);
+        System.out.println(TotalEntreprise2);
     }
 }
